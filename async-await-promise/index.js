2b31b61aa4a6b1dbd65d8944e06aed8643cdc565
shanto096 <shanto096@gmail.com>
2024-04-03 18:12:05 +0600

    promise

---
+++ async-await-promise/index.js
@@ -0,0 +1,54 @@
+const suc = false
+const mark = 90
+
+
+function enrol() {
+    console.log('class a join hoscha');
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (suc) {
+                resolve()
+            } else {
+                reject('join kora jaabe na ');
+            }
+        }, 2000);
+    })
+    return promise;
+
+}
+
+function progress() {
+    console.log('class cholca');
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (mark >= 80) {
+                resolve()
+            } else {
+                reject('tumi je mark paicho tate ami certificate dite parbo na ');
+            }
+        }, 3000);
+    })
+    return promise;
+
+
+}
+
+function certificate() {
+    console.log('valo mark paicho');
+    const promise = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('ei nau tomar certificate');
+        }, 4000);
+    })
+    return promise;
+}
+
+enrol()
+    .then(progress)
+    .then(certificate)
+    .then((res) => {
+        console.log(res)
+    })
+    .catch((err) => {
+        console.log(err);
+    })
