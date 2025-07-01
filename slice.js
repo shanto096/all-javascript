e298b6aee92b93e599da99a76ee0c74bccd8846e
shanto096 <shantokumar096@gmail.com>
2025-07-01 20:07:12 +0300

    add1

--- slice.js
+++ slice.js
@@ -1,11 +1,14 @@
- const arry = [1,2,3,4,5,6,7,8]
+ const arry = [1, 2, 3, 4, 5, 6, 7, 8]
 
-const newarry = arry.slice(3,5)
-// console.log(newarry);
-// console.log(arry);
+ const newarry = arry.slice(3, 5)
+     // console.log(newarry);
+     // console.log(arry);
 
-const removed = arry.splice(2,5, 10)
-// console.log(removed);
-// console.log(arry);
-const together = arry.join(" =")
-console.log(together);
+ const removed = arry.splice(2, 5, 10)
+     // console.log(removed);
+     // console.log(arry);
+ const together = arry.join(" =")
+ console.log(together);
+
+
+ // alll
