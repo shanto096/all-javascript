69d6d21f29e83f4c138e60f7a78b402a6e9e207d
shanto096 <shantokumar096@gmail.com>
2025-07-02 14:35:40 +0300

    add

--- interviews-question/4.js
+++ interviews-question/4.js
@@ -2,6 +2,7 @@ let nam = ['shanto', 'panto', 'sagor', 'sweet']
 
 let element = document.getElementById('printName')
 let element2 = document.getElementById('printName2')
+let element3 = document.getElementById('printName3')
 
 
 
@@ -10,4 +11,10 @@ document.getElementById('printName').addEventListener('click', () => {
 })
 document.getElementById('printName2').onclick = () => {
     element2.innerText = element2.innerText + ' ' + nam[0]
+}
+document.getElementById('printName3').onclick = () => {
+
+    for (const element of nam) {
+        element3.innerText += " " + element; // যোগ করে করে দেখাবে
+    }
 }
