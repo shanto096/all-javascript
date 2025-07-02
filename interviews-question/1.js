1102d351e9671eabc152619acc027227f1aa8e96
shanto096 <shantokumar096@gmail.com>
2025-07-02 09:44:41 +0300

    add nan

---
+++ interviews-question/1.js
@@ -0,0 +1,28 @@
+const n = NaN
+
+console.log(typeof n); //number 
+
+
+// 5	----number
+// 3.14	----number
+// Infinity--number
+// NaN	-----number
+
+
+console.log(Number("hello")); // NaN
+console.log(0 / 0); // NaN
+
+
+typeof NaN === "number" // ✅ true
+
+const v = 10;
+
+if (isNaN(v)) { //false
+    console.log('not a number'); //this is not a number  
+
+} else {
+    console.log('number ');
+
+}
+
+//NaN === NaN false হয় কারণ NaN নিজেই বলে — "আমি কারও সমান না, এমনকি নিজেরও না!"
